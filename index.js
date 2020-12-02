4ca4fd86160a07d0cf2de6ef322bf5547f917efe
Tierney Cyren <hello@bnb.im>
2020-12-02 15:03:53 -0500

    fix: use env port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fastify = require('fastify')({
 })
 
 const client = new Client('https://nodejs.org')
-const port = 3000
+const port = process.env.PORT || 3000
 
 fastify.get('/', function (request, reply) {
   client.request({
